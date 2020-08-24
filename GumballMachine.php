3915f5cc01f729709a2616c9fe631f8a0284cf97
root <root@jenkins>
2020-08-24 00:14:14 +0000

    add testing gumballs

---
+++ GumballMachine.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+class GumballMachine {
+	private $gumaballs;
+
+
+	// Get the amount of gumballs still in the machine
+	public function getGumballs(){
+		return $this->gumballs;
+	}
+
+	// Set the amount of gumballs in the machine
+	public function setGumballs($amount){
+		$this->gumballs = $amount;
+	}
+
+	// The userr turns the whell, machine dispenses gumball!
+	public function turnWhell(){
+		$this->setGumballs($this->getGumballs());
+	}
+
+}
+
+
